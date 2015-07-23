6257649abc69a674c6b6e4194fddc80f55e7db75
wangjunkai <yuyun@wangjunkaideMacBook-Pro.local>
2015-07-23 09:37:27 +0800

    添加首页动画，完善主页js

---
+++ public/modules/core/js/controller.js
@@ -0,0 +1,9 @@
+
+'use strict';
+
+bokeApp.controller('section',['$scope',function($scope){
+
+}]);
+bokeApp.controller('initsectionController',['$scope',function($scope){
+    YUYUN.yuyun.index.sectionAnimate.call(YUYUN.yuyun.index);
+}])
